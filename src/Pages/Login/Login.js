d54b2ea86c590bd08979f0ced726a10941a4c6b4
Jahid Hasan Rony <jahidrony162@gmail.com>
2023-07-14 20:09:26 +0600

    toast modify

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,89 +1,141 @@
-import React, { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthProvider';
+import React, { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const Login = () => {
-    const {signIn,loginWithGoogle} = useContext(AuthContext);
-     
-    const {handleSubmit, formState:{errors}, register} = useForm();
-    const [loginError, setLoginError] = useState('');
-    const [googleLoginError, setGoogleLoginError] = useState('');
+  const { signIn, loginWithGoogle } = useContext(AuthContext);
 
-    const location = useLocation();
-    const navigate = useNavigate();
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+  } = useForm();
+  const [loginError, setLoginError] = useState("");
+  const [googleLoginError, setGoogleLoginError] = useState("");
 
-    const from = location.state?.from?.pathname || '/';
-    const handleLogin = (data) =>{
 
-        setLoginError('');
-        //console.log(data);
-        signIn(data.email,data.password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-            navigate(from,{replace: true});
-        })
+  const from = location.state?.from?.pathname || "/";
+  const handleLogin = (data) => {
+    setLoginError("");
+    //console.log(data);
+    signIn(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        //console.log(user);
 
-        .catch(error => {
-            const errorMessage = error.message;
-            setLoginError(errorMessage);
-        })
-    }
+        navigate(from, { replace: true });
+      })
 
-    const handleGoogleLogin = ()=>{
-        setGoogleLoginError('');
-        loginWithGoogle()
-        .then((result)=>{
-             if(result.user){
-            navigate(from,{replace: true});
-             }
-        })
-        .catch((err)=> {
-            console.log(err)
-            setGoogleLoginError(err.message);
-        })
-    }
+      .catch((error) => {
+        const errorMessage = error.message;
+        setLoginError(errorMessage);
+      });
+  };
 
-    return (
-        <div className='h-[800px] flex justify-center items-center'>
-            <div className='w-96 p-7'>
-                    <h2 className='lg:text-4xl md:text-3xl sm:text-2xl text-center'>Login</h2>
-                    <form onSubmit={handleSubmit(handleLogin)}>
-                    <div className="form-control w-full ">
-                        <label className="label"> <span className="label-text">Email</span></label>
-                        <input type="email"
-                            {...register("email", {
-                                required: "Email Address is required"
-                            })}
-                            className="input input-bordered w-full focus:outline-none" />
-                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
-                    </div>
-                    <div className="form-control w-full ">
-                        <label className="label"> <span className="label-text">Password</span></label>
-                        <input type="password" {...register("password", {
-                            required: "Password is required",
-                            minLength: { value: 6, message: "Password must be 6 characters long" },
-                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
-                        })} className="input input-bordered w-ful focus:outline-none" />
-                        <label className="label"> <span className="label-text">Forget Password?</span></label>
-                        {errors.password && <p className='text-red-500'>{errors.password?.message}</p>}
-                    </div>
-                    <input className='btn btn-accent text-white w-full' value="Login" type="submit" />
-                    <div>
-                        {loginError && <p className='text-red-500'>{loginError}</p>}
-                    </div>
-                </form>
-                <p className='mt-[10px] text-[12px] text-center'>New to Doctors Portal ? <Link className='text-secondary underline' to="/signup">Create new Account</Link></p>
-                <div className="divider">OR</div>
-                <button onClick={handleGoogleLogin} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
-                    <div>
-                        {googleLoginError && <p className='text-red-500'>{googleLoginError}</p>}
-                    </div>
-            </div>
+  const handleGoogleLogin = () => {
+    setGoogleLoginError("");
+    loginWithGoogle()
+      .then((result) => {
+        if (result.user) {
+            toast.success("Login Successful");
+          navigate(from, { replace: true });
+        }
+      })
+      .catch((err) => {
+        //console.log(err)
+        setGoogleLoginError(err.message);
+      });
+  };
+
+  
+
+
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="lg:text-4xl md:text-3xl sm:text-2xl text-center">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full ">
+            <label className="label">
+              {" "}
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              
+              {...register("email", {
+                required: "Email Address is required",
+              })}
+              className="input input-bordered w-full focus:outline-none"
+            />
+            {errors.email && (
+              <p className="text-red-600">{errors.email?.message}</p>
+            )}
+          </div>
+
+          <div className="form-control w-full ">
+            <label className="label">
+              {" "}
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters long",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message:
+                    "Password must have uppercase, number and special characters",
+                },
+              })}
+              className="input input-bordered w-ful focus:outline-none"
+            />
+            <label className="label">
+              {" "}
+              <span className="label-text">Forget Password?</span>
+            </label>
+            {errors.password && (
+              <p className="text-red-500">{errors.password?.message}</p>
+            )}
+          </div>
+          <input
+            className="btn btn-accent text-white w-full"
+            value="Login"
+            type="submit"
+          />
+          <div>
+            {loginError && <p className="text-red-500">{loginError}</p>}
+          </div>
+        </form>
+        <p className="mt-[10px] text-[12px] text-center">
+          New to Doctors Portal ?{" "}
+          <Link className="text-secondary underline" to="/signup">
+            Create new Account
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button onClick={handleGoogleLogin} className="btn btn-outline w-full">
+          CONTINUE WITH GOOGLE
+        </button>
+        <div>
+          {googleLoginError && (
+            <p className="text-red-500">{googleLoginError}</p>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,64 +1,67 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
-import React, { createContext, useEffect, useState } from 'react';
-import app from '../firebase/firebase.config';
+import {
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
 export const AuthContext = createContext();
 const auth = getAuth(app);
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [user,setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const googleAuthProvider = new GoogleAuthProvider();
 
-    const googleAuthProvider = new GoogleAuthProvider();
+  const loginWithGoogle = () => {
+    return signInWithPopup(auth, googleAuthProvider);
+  };
 
-    const loginWithGoogle = ()=>{
-        return signInWithPopup(auth,googleAuthProvider);
-    }
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const createUser = (email, password) =>{
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth,email,password);
-    }
+  const updateUser = (userInfo) => {
+    return updateProfile(user, userInfo);
+  };
 
-    const signIn = (email,password) =>{
-        setLoading(true);
-        return signInWithEmailAndPassword(auth,email,password);
-    }
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    const updateUser = (userInfo) =>{
-        return updateProfile(user,userInfo)
-    }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      //console.log("user observing");
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
 
-    const logOut = () =>{
-        setLoading(true);
-        return signOut(auth);
-    }
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
-            console.log("user observing");
-            setUser(currentUser);
-            setLoading(false);
-        })
-        return ()=> unsubscribe();
-    },[])
-
-    
-
-    const authInfo = {
-        createUser,
-        signIn,
-        updateUser,
-        logOut,
-        loginWithGoogle,
-        user,
-        loading
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const authInfo = {
+    createUser,
+    signIn,
+    updateUser,
+    logOut,
+    loginWithGoogle,
+    user,
+    loading,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
