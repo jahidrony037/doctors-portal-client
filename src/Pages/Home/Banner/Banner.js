d65890200681d52d780af4706adb2810e39556d7
Jahid Hasan Rony <jahidrony162@gmail.com>
2023-07-18 20:44:34 +0600

    banner edit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -10,8 +10,8 @@ const Banner = () => {
   <div className="hero-content  flex-col lg:flex-row-reverse">
     <img src={chair} className="lg:max-w-lg md:max-w-md sm:max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <h1 className="text-5xl font-bold">Your New Smile Starts <br/> Here</h1>
+      <p className="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
